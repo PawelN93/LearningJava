d4bf046704f2b4434ee8d97d14b088eb478b4214
PawelN93 <paweln1993@gmail.com>
2023-11-27 21:09:59 +0100

    Added Exercise 4 from Module 5

---
+++ Module5/Exercise4/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final String RESOURCES = "src/resources/";
+
+    public static void main(String[] args) {
+        Path inputPath = Paths.get(RESOURCES + args[0]);
+        Path outputPath = Paths.get(RESOURCES + args[1]);
+        List<String> namesStartingWithA = new ArrayList<>();
+
+
+        try {
+            List<String> inputLines = Files.readAllLines(inputPath);
+
+            for (String line : inputLines) {
+                if (line.startsWith("A")) {
+                    namesStartingWithA.add(line);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Błąd podczas przetwarzania pliku wejściowego");
+            throw new RuntimeException(e);
+        }
+        try {
+            Files.write(outputPath, namesStartingWithA);
+        } catch (IOException e) {
+            System.out.println("Błąd podczas przetwarzania pliku wyjściowego");
+            throw new RuntimeException(e);
+        }
+    }
+}
